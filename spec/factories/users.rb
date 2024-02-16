ed7ba0494cd881f903ccc50910c113c88814c259
Cath Komrij <komcath@gmail.com>
2024-02-16 13:27:33 -0800

    Fix for warnings that showed up with rspec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,26 +1,3 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id                     :bigint           not null, primary key
-#  admin                  :boolean          default(FALSE), not null
-#  first_name             :string           default(""), not null
-#  last_name              :string           default("")
-#  phone_number           :string           default("")
-#  line1                  :string
-#  line2                  :string
-#  neighborhood           :string
-#  city                   :string
-#  state                  :string
-#  zip                    :string
-#  email                  :string           default(""), not null
-#  encrypted_password     :string           default(""), not null
-#  reset_password_token   :string
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
-#
 FactoryBot.define do
   factory :user do
     
--- spec/models/product.rb
+++ spec/models/product.rb
@@ -1,5 +1,3 @@
-require "rails_helper"
-
 RSpec.describe Product, type: :model do
   it { is_expected.to monetize(:price_cents) }
 
